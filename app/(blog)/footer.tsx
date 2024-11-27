7b558cc3d92903c91dfb3d1d7ae387515912a82d
Mads Storm Hansen <madsstorm@gmail.com>
2024-11-27 14:22:00 +0100

    remove about

--- app/(blog)/footer.tsx
+++ app/(blog)/footer.tsx
@@ -24,10 +24,20 @@ export default async function Footer() {
             <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
               Built with Next.js.
             </h3>
+
             <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
-              <Link href="/about" className="mx-3 font-bold hover:underline">
-                About
-              </Link>
+              <a
+                href="https://nextjs.org/docs"
+                className="mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8"
+              >
+                Read Documentation
+              </a>
+              <a
+                href="https://github.com/vercel/next.js/tree/canary/examples/cms-sanity"
+                className="mx-3 font-bold hover:underline"
+              >
+                View on GitHub
+              </a>
             </div>
           </div>
         )}
